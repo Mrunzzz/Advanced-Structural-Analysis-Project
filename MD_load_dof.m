6ababe2c657bd31e9a76a6970a1e65dd76f73e14
Devasmit Dutta <devasmitdutta@Devasmits-MBP.lan>
2023-12-04 13:15:07 -0800

    use default way of transposing

--- MD_load_dof.m
+++ MD_load_dof.m
@@ -1,11 +1,5 @@
 function load_dof = MD_load_dof(concen, nnodes)
-[d1, d2] = size(concen);
-load_dof = zeros(d2,d1);
 
-for i=1:d2
-    for j=1:d1
-      load_dof(i,j) = concen(j,i);
-    end
-end
+load_dof = concen';
 
 load_dof = reshape(load_dof,nnodes*6,1);
