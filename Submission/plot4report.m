78e43bae97ffb7bb1e7a22d27f1bcee97868aac6
Mrunzzz <mrunmayiggt@gmail.com>
2023-12-18 18:25:09 -0800

    Add verification problems report

--- Submission/plot4report.m
+++ Submission/plot4report.m
@@ -1,10 +1,60 @@
 figure()
+% syms x
+% L = 1.2369e+04
+% fplot(- 0.0018*x^2 + 47.7078*x - 223946.0, [0 L])
+% hold on
+% fplot([0 L/2 L],subs(- 0.0018*x^2 + 47.7078*x - 223946.0,[0 L/2 L]),"*")
+% h = plot([0 L/2 L],subs(- 0.0018*x^2 + 47.7078*x - 223946.0,[0 L/2 L]),"*")
+% 
+% 
+% xlabel("Coordinate x")
+% ylabel("Bending Moment Mz")
+
+
+% syms x
+% L = 1.2369e+04
+% fn = - 0.0018*x^2 + 165.038*x - 1094600.0;
+% fplot(fn, [0 L])
+% hold on
+% fplot([0 L/2 L],subs(fn,[0 L/2 L]),"*")
+% h = plot([0 L/2 L],subs(fn,[0 L/2 L]),"*")
+% 
+% 
+% xlabel("Coordinate x")
+% ylabel("Bending Moment Mz")
+
+% syms x
+% L = 1.2369e+04
+% fn = - 0.0018*x^2 + 78.5999*x - 388926.0;
+% fplot(fn, [0 L])
+% hold on
+% fplot([0 L/2 L],subs(fn,[0 L/2 L]),"*")
+% h = plot([0 L/2 L],subs(fn,[0 L/2 L]),"*")
+
+% syms x
+% L = 5000
+% fn = 18.9192*x - 91745.0;
+% fplot(fn, [0 L])
+% hold on
+% fplot([0 L],subs(fn,[0 L]),"*")
+% h = plot([0 L],subs(fn,[0 L]),"*")
+
+% syms x
+% L = 8000
+% fn = - 0.0025*x^2 + 18.9192*x - 877.709;
+% fplot(fn, [0 L])
+% hold on
+% xx = solve(diff(- 0.0025*x^2 + 18.9192*x - 877.709,x)==0)
+% fplot([0 xx L],subs(fn,[0 xx L]),"*")
+% h = plot([0 xx L],subs(fn,[0 xx L]),"*")
+% 
 syms x
-L = 1.2369e+04
-fplot(- 0.0018*x^2 + 47.7078*x - 223946.0, [0 L])
+L = 3000
+fn = - 21.0808*x - 2850.84;
+fplot(fn, [0 L])
 hold on
-fplot([0 L/2 L],subs(- 0.0018*x^2 + 47.7078*x - 223946.0,[0 L/2 L]),"*")
-h = plot([0 L/2 L],subs(- 0.0018*x^2 + 47.7078*x - 223946.0,[0 L/2 L]),"*")
+fplot([0 L],subs(fn,[0 L]),"*")
+h = plot([0 L],subs(fn,[0 L]),"*")
 
 
 xlabel("Coordinate x")
